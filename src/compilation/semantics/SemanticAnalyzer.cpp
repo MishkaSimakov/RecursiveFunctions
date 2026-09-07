436665cb3c9bf282dc693f8bbd0d842396b7add7
MishkaSimakov <msimakov661@gmail.com>
2026-09-07 10:32:29 +0300

    removed debug comments

--- src/compilation/semantics/SemanticAnalyzer.cpp
+++ src/compilation/semantics/SemanticAnalyzer.cpp
@@ -113,10 +113,6 @@ void SemanticAnalyzer::analyze() {
     }
   }
 
-  // ScopePrinter printer(context_.get_strings_pool(), *context_.root_scope,
-                       // std::cout);
-  // printer.print();
-
   traverse(*context_.ast_root);
 }
 }  // namespace Front
