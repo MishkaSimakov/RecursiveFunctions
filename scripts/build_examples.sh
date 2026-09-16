370764b4b6d1e4b392e033ed4ab9b53b8f1d7bed
MishkaSimakov <msimakov661@gmail.com>
2026-09-16 18:43:07 +0300

    Update CI for new stdlib location, move testing.sh to separate scripts directory

---
+++ scripts/build_examples.sh
@@ -0,0 +1,12 @@
+# This script builds every example in examples directory
+
+mkdir build
+cd build || exit 1
+cmake -DCMAKE_BUILD_TYPE=Debug .. || exit 1
+cmake --build . -t tlang || exit 1
+
+cd .. || exit 1
+
+for dir in examples/*/; do
+  (cd "$dir" && make clean && make main) || exit 1;
+done
